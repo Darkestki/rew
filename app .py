e016a6c7b89140589cb5e8332cf3bddee9ef6cf0
Darkestki <148196574+Darkestki@users.noreply.github.com>
2025-11-09 12:29:03 +0530

    Update app .py

--- app .py
+++ app .py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-filename = 'decision_tree_model.pkl'
+filename = 'random_forest_model.pkl'
 loaded_model = joblib.load(open(filename, 'rb'))
 
 
